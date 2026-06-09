c60547602798087331a2f9604a80eb10f7d53b1f
Julia Bandera <julia.bandera@gmail.com>
2026-06-09 18:22:46 +0200

    check if a parsed ligand file exists and if it has a supported extension (for now just .sdf)

--- examples/protein_plus_ligand/_run_example.sh
+++ examples/protein_plus_ligand/_run_example.sh
@@ -2,3 +2,6 @@ simprepper -r 181L_clean.pdb -l BNZ.sdf -L info --ligand_ff GAFF
 
 # this will cause an error:
 # simprepper -r 181L_clean_noTER.pdb -l BNZ.sdf -L info --ligand_ff GAFF
+
+# test for ligand extension
+# simprepper -r 181L_clean.pdb -l BNZ.mol2 -L info --ligand_ff GAFF
