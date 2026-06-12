1b6adfd7d650310aa1a4ea803a3bf851e7768987
Julia Bandera <julia.bandera@gmail.com>
2026-06-12 13:36:59 +0200

    add example for using the ini file

--- examples/protein_only/_run_example.sh
+++ examples/protein_only/_run_example.sh
@@ -1,4 +1,8 @@
 simprepper -r 181L_only_prot.pdb -L info
 
+# example with the ini file
+simprepper-to-ini # By default writes the config to default_config.ini
+simprepper -r 181L_only_prot.pdb -L info -i default_config.ini
+
 # Example to play with forcefields
 # simprepper -r 181L_only_prot.pdb -L info --protein_ff amber14/protein.ff14SB.xml --water_ff  amber14/tip3pfb.xml --ion_ff amber/tip3p_HFE_multivalent.xml --lipid_ff amber14/lipid17.xml
--- examples/protein_plus_ligand/_run_example.sh
+++ examples/protein_plus_ligand/_run_example.sh
@@ -1,5 +1,9 @@
 simprepper -r 181L_clean.pdb -l BNZ.sdf -L info --ligand_ff GAFF
 
+# example with the ini file
+simprepper-to-ini # By default writes the config to default_config.ini
+simprepper -r 181L_clean.pdb -l BNZ.sdf -L info -i default_config.ini
+
 # this will cause an error:
 # simprepper -r 181L_clean_noTER.pdb -l BNZ.sdf -L info --ligand_ff GAFF
 
