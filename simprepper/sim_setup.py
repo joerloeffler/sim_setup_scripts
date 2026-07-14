1aa37482e72d379e569c9ce4c095517a55fa7826
Julia Bandera <julia.bandera@gmail.com>
2026-07-14 17:40:44 +0200

    separate check for existence and validity of the config file

--- simprepper/sim_setup.py
+++ simprepper/sim_setup.py
@@ -99,11 +99,15 @@ class SimSetup:
         """
         This is a constructor function, to generate an instance of SimSetup() from an ini file.
         """
-        # Check if the ini file exists and if it is valid
-        sanity_check_ini_file(args.ini, CONFIG_SECTIONS)
+        # Check if the ini file exists
+        sanity_check_ini_file_if_exists(args.ini, CONFIG_SECTIONS)
 
+        # Read the config
         config = ConfigParser(inline_comment_prefixes=("#",))
         config.read(args.ini)
+
+        # Check if the ini file is valid
+        sanity_check_ini_file_is_valid(config, CONFIG_SECTIONS)
 
         defaults = cls()  # default fallback values from dataclass
 
@@ -170,29 +174,32 @@ def get_longest_key(keys):
     return longest_string+1
 
 
-def sanity_check_ini_file(ini_filename, field_sections):
+def sanity_check_ini_file_if_exists(ini_filename, field_sections):
     """
-    Checks if the ini file exists and is valid. Raises a FileNotFoundError if the file does not 
-    exist, and a UserWarning if there are unknown sections in the ini file.
+    Checks if the ini file exists. Raises a FileNotFoundError if the file does not exist.
     """
     if not os.path.isfile(ini_filename):
         raise FileNotFoundError(
             f"Provided config .ini file not found: {ini_filename}\n"
             "Use simprepper-example-config to generate a template configuration."
         )
-    else:
-        # Print a warning if there are unknown sections in the ini file, to help users identify typos or misplaced options
-        config = ConfigParser(inline_comment_prefixes=("#",))
-        config.read(ini_filename)
-        expected_sections = set(field_sections.keys())
-        actual_sections = set(config.sections())
-        unknown_sections = actual_sections - expected_sections
-        if unknown_sections:
-            warnings.warn(
-                f"Unknown section(s) ignored: {', '.join(unknown_sections)}",
-                UserWarning,
-                stacklevel=2
-            )
+
+def sanity_check_ini_file_is_valid(config_content, field_sections):
+    """
+    Checks if the ini file is valid. Raises a UserWarning if there are unknown 
+    sections in the ini file.
+    """
+    # Print a warning if there are unknown sections in the ini file, to help users identify typos or misplaced options
+    expected_sections = set(field_sections.keys())
+    actual_sections = set(config_content.sections())
+    unknown_sections = actual_sections - expected_sections
+    if unknown_sections:
+        warnings.warn(
+            f"Unknown section(s) ignored: {', '.join(unknown_sections)}",
+            UserWarning,
+            stacklevel=2
+        )
+
 
 def get_section_of_field(field_name, field_sections):
     """
