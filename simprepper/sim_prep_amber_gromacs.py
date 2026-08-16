7cc7bc7e3aca10d47667f00bab6472e3d4f14ae0
Patrick Quoika <Patrick.Quoika@uibk.ac.at>
2026-08-16 16:42:18 +0200

    code snippet for DEBUG of development
    This is really only to test, if the configsection classes work as
    intended...

--- simprepper/sim_prep_amber_gromacs.py
+++ simprepper/sim_prep_amber_gromacs.py
@@ -17,12 +17,8 @@ from simprepper.argument_parsing import parser
 from simprepper.utils import select_platform, get_sysname, prep_filetree, export_all_files, sanity_check_pdb_for_TERs, sanity_check_ligand_extension, check_initial_box_dimensions, calculate_protein_dimensions
 from simprepper.utils import ExportPathManager
 from simprepper.structure_prep import prepare_ligand, prepare_protein, parametrize_ligand
-<<<<<<< HEAD
 from simprepper.sim_setup import SimSetup
 from simprepper.setup_checker import SetupChecker
-=======
-from simprepper.sim_setup import SimSetup, BoxSection
->>>>>>> prototype for ConfigSections
 
 # OpenMM imports
 import openmm
@@ -130,7 +126,6 @@ def main():
 
     logging.info("Modelling the system...")
 
-<<<<<<< HEAD
     # Create an OpenMM ForceField object with parsed forcefields
     if setup.lipid_ff is None:
         forcefield = mm_apps.ForceField(
@@ -148,16 +143,6 @@ def main():
         )
         logging.info(f"Using {setup.protein_ff}, {setup.water_ff}, {setup.ion_ff}, and {setup.lipid_ff} forcefields.")
     
-<<<<<<< HEAD
-=======
-=======
-    # Create an OpenMM ForceField object with parsed forcefields 
-    forcefield = mm_apps.ForceField(
-        setup.protein_ff,
-        setup.water_ff,
-        setup.ion_ff,
-        setup.lipid_ff,
-    )
     # 1. Instantiate default values directly via from_defaults()
     box = BoxSection.from_defaults()
 
@@ -176,13 +161,11 @@ def main():
     print("Quitting debugging run...")
     quit()
     logging.info(f"Using {setup.protein_ff}, {setup.water_ff}, {setup.ion_ff}, and {setup.lipid_ff} forcefields.")
->>>>>>> prototype for ConfigSections
 
     # Make an OpenMM Modeller object with the protein
     sys_modeller = mm_apps.Modeller(pdb_fixed.topology, 
                                     pdb_fixed.positions)
 
->>>>>>> prototype for ConfigSections
     # Optional ligand
     if setup.lig_fname:
         #NOTE: `ligand` is not acually used anywhere anymore
